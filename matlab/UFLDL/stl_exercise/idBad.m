4638d77f8079444b17b5c85881649fee85cf8d07
YungKC <kaicyung@gmail.com>
2014-09-29 16:18:20 -0700

    fixed bug

--- matlab/UFLDL/stl_exercise/idBad.m
+++ matlab/UFLDL/stl_exercise/idBad.m
@@ -1,13 +1,13 @@
 index = 0;
-toView = zeros(784,100);
+toView = zeros(784,40);
 for n=1:15298
 	if pred(n) ~= testLabels(n)
 		index = index+1;
-		disp(sprintf('%d %d %d %d', index, n, pred(n), trainLabels(n)));
+		disp(sprintf('%d %d %d %d', index, n, pred(n)-1, testLabels(n)-1));
 		toView(:,index) = testData(:,n);
-%		if index >= 100
-%			break;
-%		end
+		if index >= 40
+			break;
+		end
 	end
 end
 display_network(toView);
