be04dbc28b288796481af023756be7a9609b468e
YungKC <kaicyung@gmail.com>
2014-12-17 22:58:35 -0800

    still working on it

--- convnetjs_release/demo/cifar-10-batches-py/augmentImage.py
+++ convnetjs_release/demo/cifar-10-batches-py/augmentImage.py
@@ -8,8 +8,12 @@ import subprocess
 srcFiles = [os.getcwd()+'/../chiquita/ducky2.jpg', os.getcwd()+'/../chiquita/hawaii2.jpg', os.getcwd()+'/../chiquita/kid2.jpg', os.getcwd()+'/../chiquita/maid2.jpg', os.getcwd()+'/../chiquita/santa2.jpg', os.getcwd()+'/../chiquita/stash2.jpg']
 
 random.seed()
+xs = []
+ys = []
+numImages = 1000
+outFile = os.getcwd()+'/../chiquita/out/tmp.png'
 
-for i in range(1):
+for i in range(numImages):
 	index = random.randint(0,5)
 	srcFile = srcFiles[index]
 	angle = random.randint(-10,10)
@@ -18,10 +22,24 @@ for i in range(1):
 	seedVal = random.randint(0,10000)
 	attVal = random.randint(0,100)*0.01
 
-	outFile = os.getcwd()+'/../chiquita/out/tmp'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
-	print outFile
+#	outFile = os.getcwd()+'/../chiquita/out/tmp'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
+#	outLongFile = os.getcwd()+'/../chiquita/out/tmpL'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
+#	print outFile
 
 	subprocess.call(['convert',srcFile,'-rotate',`angle`,'-morphology','Convolve','Comet:0x'+`cometVal`+'+'+`cometRot`,'-seed',`seedVal`,'-attenuate',`attVal`,'+noise','gaussian','-gravity','Center','-crop','32x32+0+0','+repage',outFile])
 
+	imageData = imread(outFile)
+	print numpy.shape(imageData)
+	xs.append(imageData)
+	ys.append([index])
 
+x = numpy.concatenate(xs)
+y = numpy.concatenate(ys)
+
+outLongFile = os.getcwd()+'/../chiquita/out/tmpL.png'
+
+imageDataReshaped = numpy.reshape(x,(numImages,32*32,3))
+print numpy.shape(imageDataReshaped)
+imsave(outLongFile, imageDataReshaped)
+	
 
