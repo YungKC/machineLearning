1e24cdc0ffefe3614b5a1b756af098c74276395c
YungKC <kaicyung@gmail.com>
2014-12-22 23:03:26 -0800

    almost there

--- convnetjs_release/demo/cifar-10-batches-py/augmentImage.py
+++ convnetjs_release/demo/cifar-10-batches-py/augmentImage.py
@@ -17,21 +17,27 @@ ys = []
 numImages = 1000
 outFile = os.getcwd()+'/../chiquita/out/tmp.png'
 
+rotAngle = 0
 for i in range(numImages):
-	index = random.randint(0,5)
-	srcFile = srcFiles[index]
-	angle = random.randint(-10,10)
-	cometVal = random.randint(0, 1)
-	cometRot = random.randint(0,3)*90
-	seedVal = random.randint(0,10000)
-	attVal = random.randint(0,100)*0.01
+	index = random.randint(0,20)		# if index between 0 and 5, then use target images, otherwise, use random image as negative case.
+	if index <= 5:
+		srcFile = srcFiles[index]
+		angle = random.randint(-10,10)
+		cometVal = random.randint(0, 1)
+		cometRot = random.randint(0,3)*90
+		seedVal = random.randint(0,10000)
+		attVal = random.randint(0,100)*0.01
 
-#	outFile = os.getcwd()+'/../chiquita/out/tmp'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
-#	outLongFile = os.getcwd()+'/../chiquita/out/tmpL'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
-#	print outFile
+	#	outFile = os.getcwd()+'/../chiquita/out/tmp'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
+	#	outLongFile = os.getcwd()+'/../chiquita/out/tmpL'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
+	#	print outFile
 
-	subprocess.call(['convert',srcFile,'-rotate',`angle`,'-morphology','Convolve','Comet:0x'+`cometVal`+'+'+`cometRot`,'-seed',`seedVal`,'-attenuate',`attVal`,'+noise','gaussian','-gravity','Center','-crop','32x64+0+0','+repage',outFile])
-
+		subprocess.call(['convert',srcFile,'-rotate',`angle`,'-morphology','Convolve','Comet:0x'+`cometVal`+'+'+`cometRot`,'-seed',`seedVal`,'-attenuate',`attVal`,'+noise','gaussian','-gravity','Center','-crop','32x64+0+0','+repage',outFile])
+	else:
+		fileNum = random.randint(0,99)
+		offset = random.randint(0,32)
+		index=6
+		subprocess.call(['convert', 'kai/cifar10_batch_'+`fileNum`+'.png', '-resize', '64x64', '-crop', '32x64+'+`offset`+'+0', outFile])
 	imageData = imread(outFile)
 #	print numpy.shape(imageData)
 	sys.stdout.write('.')
