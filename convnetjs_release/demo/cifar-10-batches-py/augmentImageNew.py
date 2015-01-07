5ab7df104315d3b40ecdf7ec5bf58d1cbe2c8f35
YungKC <kaiyung@comcast.net>
2015-01-06 22:11:30 -0800

    update augmentedImageNew.py to fix bugs

--- convnetjs_release/demo/cifar-10-batches-py/augmentImageNew.py
+++ convnetjs_release/demo/cifar-10-batches-py/augmentImageNew.py
@@ -10,7 +10,7 @@ import os
 
 millis = int(round(time.time() * 1000))
 
-srcFiles = [os.path.join(os.getcwd(),'..','chiquita','ducky3.jpg'), os.path.join(os.getcwd(),'..','chiquita','hawaii3.jpg'), os.path.join(os.getcwd(),'..','chiquita','kid3.jpg'),os.path.join(os.getcwd(),'..','chiquita','maid3.jpg'), os.path.join(os.getcwd(),'..','chiquita','santa3.jpg'), os.path.join(os.getcwd(),'..','chiquita','stash3.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoDucky.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoHawaii.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoKid.jpg'), os.path.join(os.getcwd(),'..','chiquita','chiquita/logo.jpg'), os.path.join(os.getcwd(),'..','chiquita','logo1.jpg'), os.path.join(os.getcwd(),'..','chiquita','logo2.jpg')]
+srcFiles = [os.path.join(os.getcwd(),'..','chiquita','ducky3.jpg'), os.path.join(os.getcwd(),'..','chiquita','hawaii56x100screenshot.jpg'), os.path.join(os.getcwd(),'..','chiquita','kid3.jpg'),os.path.join(os.getcwd(),'..','chiquita','maid3.jpg'), os.path.join(os.getcwd(),'..','chiquita','santa3.jpg'), os.path.join(os.getcwd(),'..','chiquita','stash3.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoDucky.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoHawaii.jpg'), os.path.join(os.getcwd(),'..','chiquita','logoKid.jpg'), os.path.join(os.getcwd(),'..','chiquita','logo.jpg'), os.path.join(os.getcwd(),'..','chiquita','logo1.jpg'), os.path.join(os.getcwd(),'..','chiquita','logo2.jpg')]
 
 random.seed()
 ys = []
@@ -37,29 +37,29 @@ for loopCount in range(10):
 		#	outLongFile = os.getcwd()+'/../chiquita/out/tmpL'+`index`+'_'+`angle`+'_'+`cometVal`+'_'+`cometRot`+'_'+`seedVal`+'_'+`attVal`+'.jpg'
 		#	print outFile
 
-			subprocess.call(['convert',srcFile,'+level',`level`,'-rotate',`angle`,'-morphology','Convolve','Comet:0x'+`cometVal`+'+'+`cometRot`,'-seed',`seedVal`,'-attenuate',`attVal`,'+noise','gaussian','-gravity','Center','-crop','32x64+0+0','+repage',outFile], shell=True)
+			subprocess.call(['convert',srcFile,'+level',`level`,'-rotate',`angle`,'-morphology','Convolve','Comet:0x'+`cometVal`+'+'+`cometRot`,'-seed',`seedVal`,'-attenuate',`attVal`,'+noise','gaussian','-gravity','Center','-crop','32x64+0+0','+repage',outFile])
 		elif index < 23:
 			srcFile = srcFiles[index%6]
 			index=6
-			xoffset = random.randint(0,6) + 10
-			yoffset = random.randint(0,9) + 10
+			xoffset = random.randint(0,6) + 4
+			yoffset = random.randint(0,9) + 6
 			offsetSign = random.randint(0,3)
 			index=6
 			if offsetSign == 0:
-				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64+'+`xoffset`+'+'+`yoffset`, outFile], shell=True)
+				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64+'+`xoffset`+'+'+`yoffset`, outFile])
 			elif offsetSign == 1:
-				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64+'+`xoffset`+'-'+`yoffset`, outFile], shell=True)
+				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64+'+`xoffset`+'-'+`yoffset`, outFile])
 			elif offsetSign == 2:
-				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64-'+`xoffset`+'+'+`yoffset`, outFile], shell=True)
+				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64-'+`xoffset`+'+'+`yoffset`, outFile])
 			elif offsetSign == 3:
-				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64-'+`xoffset`+'-'+`yoffset`, outFile], shell=True)
+				subprocess.call(['convert', srcFile,'-gravity','Center','-crop', '32x64-'+`xoffset`+'-'+`yoffset`, outFile])
 		else:
 			fileNum = random.randint(0,99)
 			offset = random.randint(0,32)
 			index=6
-			subprocess.call(['convert', os.path.join('kai','cifar10_batch_'+`fileNum`+'.png'), '-resize', '64x64', '-crop', '32x64+'+`offset`+'+0', outFile], shell=True)
+			subprocess.call(['convert', os.path.join('kai','cifar10_batch_'+`fileNum`+'.png'), '-resize', '64x64', '-crop', '32x64+'+`offset`+'+0', outFile])
 		imageData = imread(outFile)
-	#	print numpy.shape(imageData)
+		# print numpy.shape(imageData)
 		sys.stdout.write('.')
 		sys.stdout.flush()
 		xs.append(imageData)
